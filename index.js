0e2b4b2a256b4b819bbf00f18b5fb9232ff9d816
KellyMark <kmark602@gmail.com>
2023-01-24 16:40:00 -0500

    commit tree 04aeb5afd55858e89cf9fbe2fa9ab883230cfa4c

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    string.replace(/[^\w\s-']/gi, '');
+  }
+
+  static titleize(string) {
+    let words = string.split(" ");
+    for (const word of words) {
+      if (word === words[0]) {
+        words[words.indexOf(word)] = word[0].toUpperCase() + word.substring(1);
+      }
+      console.log(word);
+      if (word !== "a" && word !== "the" && word !== "an" && word !== "but" && 
+      word !== "of" && word !== "and" && word !== "for" && 
+      word !== "at" && word !== "by" && word !== "from") {
+        words[words.indexOf(word)] = word[0].toUpperCase() + word.substring(1);
+    }
+  }
+  
+  words = words.join(" ");
+  console.log(words);
+    return words;
+  }  
 }
+
+
+Formatter.titleize("hi my name is kelly and i am from connecticut");
